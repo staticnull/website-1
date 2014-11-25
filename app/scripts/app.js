f58b08fe9e0b352f6252526533102f271707fc19
Soeren Berg Glasius <soeren@glasius.dk>
2014-11-24 20:51:17 -0800

    GR8Conf US website

---
+++ app/scripts/app.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gr8app2015App
+ * @description
+ * # gr8app2015App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gr8conf2015', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'smoothScroll',
+    'angular-parallax',
+    'angular-carousel',
+    'config'
+  ])
+  .config(function ($routeProvider, CONFERENCE) {
+    console.debug(CONFERENCE);
+    var base = 'views/' + CONFERENCE.base;
+    $routeProvider
+      .when('/', {
+        templateUrl: base + '/front.html',
+        controller: 'FrontCtrl',
+        scroll: 'top'
+      })
+      .when('/front', {
+        templateUrl: base + '/front.html',
+        controller: 'FrontCtrl',
+        scroll: 'page'
+      })
+      .when('/venue', {
+        templateUrl: base + '/venue.html',
+        controller: 'VenueCtrl',
+        scroll: 'page'
+
+      })
+      .when('/cfp', {
+        templateUrl: base + '/cfp.html',
+        controller: 'CfpCtrl',
+        scroll: 'page'
+
+      })
+      .when('/about', {
+        templateUrl: base + '/about.html',
+        controller: 'AboutCtrl',
+        scroll: 'page'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+angular
+  .module('gr8conf2015').run(['$rootScope', 'smoothScroll','CONFERENCE', function($rootScope, smoothScroll, CONFERENCE) {
+    $rootScope.scrollToTop = function() {
+      var element = document.getElementById('page');
+      smoothScroll(element, {offset: 50});
+    };
+    $rootScope.eventImageCss = 'event-'+CONFERENCE.base;
+    $rootScope.logoUrl = 'views/'+CONFERENCE.base + "/templates/logo.html";
+    $rootScope.menuUrl = 'views/'+CONFERENCE.base+"/templates/menu.html";
+    $rootScope.eventUrl = 'views/'+CONFERENCE.base+"/templates/event.html";
+    $rootScope.blogUrl = 'views/'+CONFERENCE.base+"/templates/blog.html";
+    $rootScope.socialsUrl = 'views/'+CONFERENCE.base+"/templates/socials.html";
+    $rootScope.contactsUrl = 'views/'+CONFERENCE.base+"/templates/contacts.html";
+
+  }]);
---
+++ app/scripts/controllers/front.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gr8app2015App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gr8app2015App
+ */
+angular.module('gr8conf2015')
+  .controller('FrontCtrl', function ($scope, ENV) {
+    console.debug(ENV);
+  });
