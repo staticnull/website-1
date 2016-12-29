671c8b7f9277b72a71f0aa7b939673a5254b4b85
Eric Helgeson <erichelgeson@gmail.com>
2016-12-29 09:54:12 -0600

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-ssh -t gr8confus bash -c "'
+set -e
 
-sudo service gr8confus-website stop
-sudo rm /etc/init.d/gr8confus-website
+./gradlew clean test assemble
 
-cd /home/centos/gr8confus-website-source
-rm -rf build
-git pull
-./gradlew assemble
-
-sudo rm /home/centos/gr8confus-website-running/gr8confus-website.jar
-cp /home/centos/gr8confus-website-source/build/libs/gr8confus-website.jar /home/centos/gr8confus-website-running/gr8confus-website.jar
-chmod 500 /home/centos/gr8confus-website-running/gr8confus-website.jar
-
-sudo ln -s /home/centos/gr8confus-website-running/gr8confus-website.jar /etc/init.d/gr8confus-website
-sudo service gr8confus-website start
-
-'"
+eb deploy
 
 echo "GR8CONF US Deploy Finished"
